11b3f55620bd75181511effc4d5487d067255c19
Mateus Alves <msilva.spa@hotmail.com>
2020-01-22 00:02:22 -0300

    finalizado front end web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -13,8 +14,9 @@ mongoose.connect(
 	}
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const PORT = 3000;
-app.listen(3000, console.log('Server running on port: ' + PORT));
+const PORT = 3333;
+app.listen(PORT, console.log('Server running on port: ' + PORT));
